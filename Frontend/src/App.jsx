833e289363a1fc7368470ed16b0dba0524a0ebc4
Emilio <emiliojoaquinortizmalich@gmail.com>
2025-04-21 23:29:01 -0300

    cambios guardados para subirlo a vercel

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/navbar';
+import DinoCharts from './components/dinoChart';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <DinoCharts />
+    </div>
+  );
+}
+
+export default App;
